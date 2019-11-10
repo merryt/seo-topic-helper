16b006a22353cb1902fad0594cc5bd25710b7346
tmerry <tymerry@gmail.com>
2019-11-10 15:28:06 -0700

    roughed in google bit

---
+++ searchybit.py
@@ -0,0 +1,32 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import os
+import string
+
+recomendations = []
+def top_five_suggestions(results):
+    for recomendation in results:
+        recomendations.append(recomendation.text)
+
+url = "http://google.com"
+driver = webdriver.Firefox()
+driver.get(url)
+
+for letter in string.ascii_lowercase[:3]:
+    search_box = driver.find_element_by_xpath("//*[@title=\"Search\"]")
+    search_box.clear();
+    for char in f"digital nomad web developer {letter}":
+        search_box.send_keys(char)
+
+    results_box = driver.find_element_by_xpath("//*[@role=\"listbox\"]")
+    time.sleep(2)
+    results = results_box.find_elements_by_tag_name("span")
+    top_five_suggestions(results)
+
+
+print(recomendations)
+
+
+driver.quit()
